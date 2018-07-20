3ede138597eea997aecbaefa851895f8642e872a
Kuei Peng <ragnakuei@livemail.tw>
2018-07-21 00:10:44 +0800

    Country 部份功能完成

---
+++ EntityFrameworkDemo/Log/LogAdapter.cs
@@ -0,0 +1,15 @@
+﻿using NLog;
+
+namespace EntityFrameworkDemo.Log
+{
+    public class LogAdapter
+    {
+        private ILogger _logger;
+
+        public void Initial<T>()
+        {
+            _logger = LogManager.GetLogger(typeof(T).FullName);
+        }
+
+    }
+}
